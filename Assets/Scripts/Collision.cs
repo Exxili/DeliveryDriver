1004fc507364928698b662a989bae8c300f48dee
Exxili <sirvaux@live.com>
2022-06-12 12:22:28 +0100

    Added art assets and setup pixels per unit scaling

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -8,4 +8,9 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("Collision Detected! - Ouch");
     }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("Trigger fired! - Pickup collected");
+    }
 }
