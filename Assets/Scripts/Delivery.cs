2de1f03c21ff6498db0118d5ed07577189f8d26f
Exxili <sirvaux@live.com>
2022-06-12 16:28:17 +0100

    implemented boost pickup and slowdown on collision trigger

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    public Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    public Color32 baseColor = new Color32(1, 1, 1, 1);
+
+    SpriteRenderer spriteRenderer;
+
+    bool hasPackage;
+
+    public float DestroyDelay = 1f;
+
+    private void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+    
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Collision Detected! - Ouch");
@@ -12,15 +26,30 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) 
     {
         // If car collides with package, print "Got package" to console
-        if (other.tag == "Package") 
+        if (other.tag == "Package" && !hasPackage) 
         {
             Debug.Log("Got Package!");
+            hasPackage = true;
+
+            // Update the color of the car to indicate you have picked up a package
+            spriteRenderer.color = hasPackageColor;
+
+            // Remove the Package from the world
+            Destroy(other.gameObject, DestroyDelay);
+
+
+            
         }
 
         // if car collides with customer, print "Delivered Package" to console
-        if (other.tag == "Customer") 
+        if (other.tag == "Customer" && hasPackage) 
         {
             Debug.Log("Delivered Package");
+            
+        // Update the color of the car to indicate you have delivered up a package
+            spriteRenderer.color = baseColor;
+
+            hasPackage = false;
         }
     }
 }
--- Assets/Scripts/DriverScript.cs
+++ Assets/Scripts/DriverScript.cs
@@ -10,13 +10,9 @@ public class DriverScript : MonoBehaviour
     // Public variables can also be used, but this makes the
     // variable available to other scripts in the unity project
     public float steerSpeed = 1f;
-    public float moveSpeed = 0.01f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 20f;
+    public float slowSpeed = 10f;
+    public float boostSpeed = 30f;
 
     // Update is called once per frame
     void Update()
@@ -26,5 +22,24 @@ public class DriverScript : MonoBehaviour
 
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Oops Hit a slowdown");
+            moveSpeed = slowSpeed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        if (other.tag == "Boost")
+        {
+            Debug.Log("Hit A Booster");
+            moveSpeed = boostSpeed;    
+        } 
     }
 }
